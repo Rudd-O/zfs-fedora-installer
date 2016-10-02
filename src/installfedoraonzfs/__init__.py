536d5621183e05a10cd3eb2c1f9237a49b9edea2
Manuel Amador (Rudd-O) <rudd-o@rudd-o.com>
2016-10-02 08:08:49 +0000

    add debug log lovel for systemd during test boot

--- src/installfedoraonzfs/__init__.py
+++ src/installfedoraonzfs/__init__.py
@@ -1245,7 +1245,7 @@ GRUB_PRELOAD_MODULES='part_msdos ext2'
         proper_timeout = qemu_timeout * qemu_full_emulation_factor
         logging.warning("No hardware (KVM) emulation available.  The next step is going to take a while.")
     dracut_cmdline = ("rd.info rd.debug rd.udev.debug systemd.show_status=1 "
-                      "systemd.log_target=console systemd.log_level=info")
+                      "systemd.log_target=console systemd.log_level=debug")
     if interactive_qemu:
         screenmode = [
             "-nographic"
