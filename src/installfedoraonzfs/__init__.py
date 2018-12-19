dce268016f67bb5314d38166ab44b890a33f91c3
Manuel Amador (Rudd-O) <rudd-o@rudd-o.com>
2018-12-19 06:46:35 +0000

    Reduce log output.

--- src/installfedoraonzfs/__init__.py
+++ src/installfedoraonzfs/__init__.py
@@ -940,10 +940,12 @@ mount
 rm -f /boot/grub2/grubenv /boot/efi/EFI/fedora/grubenv
 ln -s /boot/efi/EFI/fedora/grubenv /boot/grub2/grubenv
 echo "# GRUB Environment Block" > /boot/efi/EFI/fedora/grubenv
+set +x
 for x in `seq 999`
 do
     echo -n "#" >> /boot/efi/EFI/fedora/grubenv
 done
+set -x
 grub2-install /dev/sda
 grub2-mkconfig -o /boot/grub2/grub.cfg
 cat /boot/grub2/grub.cfg > /boot/efi/EFI/fedora/grub.cfg
