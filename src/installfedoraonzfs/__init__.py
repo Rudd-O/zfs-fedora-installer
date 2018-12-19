6010450099439dca86f620b063c602ba57bc58ee
Manuel Amador (Rudd-O) <rudd-o@rudd-o.com>
2018-12-19 10:51:32 +0000

    TimeFormatter and improvement of output.

--- src/installfedoraonzfs/__init__.py
+++ src/installfedoraonzfs/__init__.py
@@ -34,22 +34,33 @@ BASE_PACKAGES = ("basesystem rootfiles bash nano binutils rsync NetworkManager "
                  "rpm vim-minimal e2fsprogs passwd pam net-tools cryptsetup "
                  "kbd-misc kbd policycoreutils selinux-policy-targeted "
                  "libseccomp util-linux sed pciutils").split()
-BASIC_FORMAT = '%(levelname)8s:%(name)16s  %(message)s'
-TRACE_FORMAT = '%(asctime)s  %(levelname)8s:%(name)16s:%(funcName)32s@%(lineno)4d\t%(message)s'
+BASIC_FORMAT = '%(asctime)8s  %(levelname)2s  %(message)s'
+TRACE_FORMAT = '%(asctime)8s  %(levelname)2s:%(name)16s:%(funcName)32s@%(lineno)4d\t%(message)s'
 qemu_timeout = 180
 
 
 def log_config(trace_file=None):
+    logging.addLevelName(logging.DEBUG, "TT")
+    logging.addLevelName(logging.INFO, "II")
+    logging.addLevelName(logging.WARNING, "WW")
+    logging.addLevelName(logging.ERROR, "EE")
+    logging.addLevelName(logging.CRITICAL, "XX")
+    class TimeFormatter(logging.Formatter):
+        def __init__(self, *a, **kw):
+            logging.Formatter.__init__(self, *a, **kw)
+            self.start = time.time()
+        def formatTime(self, record, datefmt):
+            return "%.2f" % (time.time() - self.start)
     rl = logging.getLogger()
     rl.setLevel(logging.DEBUG)
     ch = logging.StreamHandler()
-    cfm = logging.Formatter(BASIC_FORMAT)
+    cfm = TimeFormatter(BASIC_FORMAT)
     ch.setLevel(logging.INFO)
     ch.setFormatter(cfm)
     rl.addHandler(ch)
     if trace_file:
         th = logging.FileHandler(trace_file, mode="w")
-        tfm = logging.Formatter(TRACE_FORMAT)
+        tfm = TimeFormatter(TRACE_FORMAT)
         th.setLevel(logging.DEBUG)
         th.setFormatter(tfm)
         rl.addHandler(th)
