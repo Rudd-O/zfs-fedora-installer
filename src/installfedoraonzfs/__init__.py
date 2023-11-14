ffaa3663c9ce5a488d0f6a27d2cd59d7bdeac733
Manuel Amador (Rudd-O) <rudd-o@rudd-o.com>
2023-11-14 13:17:14 +0000

    Add missing arch.

--- src/installfedoraonzfs/__init__.py
+++ src/installfedoraonzfs/__init__.py
@@ -1875,7 +1875,7 @@ def deploy_zfs_in_machine(
                 [
                     "bash",
                     "-xc",
-                    "for kver in $(rpm -q kernel-devel --queryformat='%{version}-%{release} ')"
+                    "for kver in $(rpm -q kernel-devel --queryformat='%{version}-%{release}.%{arch} ')"
                     " ; do dkms install -k $kver || exit $? ; "
                     "done",
                 ]
